4266b3b12b95440f8c5f3c3d893f28aed7ddd525
Tyamsp <tsuchiya-daiki-xv@ynu.jp>
2017-08-13 20:00:23 +0900

    1.5.6 finish

--- Chapter1/NumPyTutorial.py
+++ Chapter1/NumPyTutorial.py
@@ -19,5 +19,23 @@ print(A)
 B = np.array([[3, 0],[0,6]])
 print("A + B =\n", A + B)
 print("A * B =\n", A * B)
+print("A * 10 =\n", A * 10)
 
+# numpy broadcast
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print("A * B = \n", A * B)
 
+# Access array
+X = np.array([[51, 55],[14,19],[0,4]])
+print("X = \n", X)
+print("X[0] = ", X[0])
+print("X[0][1] = ", X[0][1])
+
+# for
+print("for i in X: print(i)")
+for i in X:
+    print(i)
+X = X.flatten()
+print(X)
+print(X[np.array([0,2,4])])
