99380017542272665f9f1f6efa3e0ed36154e51e
Tyamsp <tsuchiya-daiki-xv@ynu.jp>
2017-08-25 09:12:31 +0900

    relu

--- Chapter3/NN.py
+++ Chapter3/NN.py
@@ -9,9 +9,14 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
 
 def creategraph(x, func):
     y = func(x)
     plt.plot(x, y)
     plt.show()
+
+def init_network(nx,nw_array,ny):
+    return 0
 
