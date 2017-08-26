eed4406139ae1e9c0895b6ea5cb3a42f1b500154
Tyamsp <tsuchiya-daiki-xv@ynu.jp>
2017-08-27 03:17:15 +0900

    implemented init_network, forward

--- Chapter3/NN.py
+++ Chapter3/NN.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pylab as plt
+import numpy as np
 
 
 def step_function(x):
@@ -9,14 +9,41 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def relu(x):
     return np.maximum(0, x)
 
+
+# グラフの作成
 def creategraph(x, func):
     y = func(x)
     plt.plot(x, y)
     plt.show()
 
-def init_network(nx,nw_array,ny):
-    return 0
 
+# 恒等関数
+def identity_function(x):
+    return x
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+    return y
